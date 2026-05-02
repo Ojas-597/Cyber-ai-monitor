a26a068b2e8439a6ffe1f8a275a5ce74f8f151db
Ojaswita Ranjit Desai <ojaswitadesai@gmail.com>
2026-05-02 13:03:39 +0530

    Update auth.py

--- modules/auth.py
+++ modules/auth.py
@@ -1,6 +1,18 @@
 from flask_login import UserMixin
 
+users = {
+    "admin": {"password": "admin123", "role": "admin"},
+    "user": {"password": "user123", "role": "user"},
+    "guest": {"password": "guest123", "role": "guest"}
+}
+
 class User(UserMixin):
-    def __init__(self, username, role):
+    def __init__(self, username):
         self.id = username
-        self.role = role
+        self.password = users[username]["password"]
+        self.role = users[username]["role"]
+
+def get_user(username):
+    if username in users:
+        return User(username)
+    return None
